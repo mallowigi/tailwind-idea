9f982ff1a5c04bf6b09a0a489b610166b433dc64
Elior <heliosaian@gmail.com>
2021-11-06 20:40:31 +0200

    cleanup [main]

--- src/main/kotlin/com/github/jsbeckr/tailwindidea/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/jsbeckr/tailwindidea/listeners/MyProjectManagerListener.kt
@@ -1,12 +1,12 @@
 package com.github.jsbeckr.tailwindidea.listeners
 
+import com.github.jsbeckr.tailwindidea.services.TailwindService
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import com.github.jsbeckr.tailwindidea.services.TailwindService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
-    override fun projectOpened(project: Project) {
-        project.getService(TailwindService::class.java)
-    }
+  override fun projectOpened(project: Project) {
+    project.getService(TailwindService::class.java)
+  }
 }
